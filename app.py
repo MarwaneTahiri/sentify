6a177d1b2d8a9a1bef6ee31fd218b36699d81086
Mawrwane Tahiri <marwanetahiripro@gmail.com>
2026-05-20 18:57:10 +0100

    add analyse route to handle form submission

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/analyse', methods=["POST"])
+def analyse():
+    text = request.form.get('text')
+    return f"you returned {text}"
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
