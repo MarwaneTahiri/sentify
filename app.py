f0092e9af28d5eccc2037a58fe36d21a2097348f
Mawrwane Tahiri <marwanetahiripro@gmail.com>
2026-06-11 16:09:13 +0100

    add SQLite database to store analysis history

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, request, redirect
 from transformers import pipeline
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime
+import datetime
+
 
 analyzer = pipeline("sentiment-analysis")
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sentify.db"
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
+
+
+class Analysis(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column()
+    label: Mapped[str] = mapped_column()
+    score: Mapped[float] = mapped_column()
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(), default=datetime.datetime.now())
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def home():
@@ -19,10 +45,17 @@ def analyse():
     
     label = sentiment[0]['label']
     score = sentiment[0]['score']
+
+    analysis = Analysis(text = text, label = label, score = score)
+    db.session.add(analysis)
+    db.session.commit()
     
     return render_template("result.html", score = round(score * 100, 2), label = label, text = text)
 
-        
+@app.route('/history')
+def history():
+    analyses = Analysis.query.all()
+    return str([(a.text, a.label, a.score) for a in analyses])        
 
 if __name__ == '__main__':
     app.run(debug=True)
